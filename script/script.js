27972f5c50c6510c031b7a76a2a9510c37af58a5
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-04-13 16:40:56 +0600

    fix the null message

--- script/script.js
+++ script/script.js
@@ -16,22 +16,32 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
-//   {
-//     "id": 3,
-//     "level": 2,
-//     "word": "Cautious",
-//     "meaning": "সতর্ক",
-//     "pronunciation": "কশাস"
-// }
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+         <div class="col-span-full text-center font-bangla space-y-5">
+         <img class="mx-auto" src="./assets/alert-error.png" alt="">
+            <p class="text-gray-400 text-xs">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-semibold text-3xl">নেক্সট Lesson এ যান</h2>
+        </div>
+    `;
+  }
+
+  //   {
+  //     "id": 3,
+  //     "level": 2,
+  //     "word": "Cautious",
+  //     "meaning": "সতর্ক",
+  //     "pronunciation": "কশাস"
+  // }
 
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
-    card.innerHTML=`
+    card.innerHTML = `
     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-5">
-            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <h2 class="text-2xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="text-2xl font-medium">Meaning /Pronounciation</p>
-            <div class="font-bangla text-2xl font-medium">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="font-bangla text-2xl font-medium">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি "} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি"}</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
