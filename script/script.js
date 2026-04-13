16cc4a6ddafc6dbae0c77bf99faf3182ad5202cf
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-04-13 15:01:48 +0600

    added level API and apply style

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+  // 1. get the Element and empty it
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  // 2. get every lessons
+  for (let lesson of lessons) {
+    // 3. create Element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>lesson - ${lesson.level_no}</button>
+    `;
+    // 4. append into container
+    levelContainer.append(btnDiv);
+  }
+};
+loadLessons();
